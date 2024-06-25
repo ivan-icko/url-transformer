c6ec34c6a9da8bec3afb6a566b3d9762532d6fe3
icko <ivanstepanovic6@gmail.com>
2024-06-25 20:13:36 +0200

    feat: Add error code constants

---
+++ src/common/constants/error-codes.ts
@@ -0,0 +1,11 @@
+export const GENERAL_ERROR_CODE = '000';
+export const FORBIDDEN_ACCESS = '001';
+
+export const REST_SERVICE = '039';
+export const GENERIC_REST_EXCEPTION = '040';
+
+export const AUTH_BAD_REQUEST = '301';
+
+// validation
+export const BAD_REQUEST_ERROR = '350';
+export const UNPROCESSABLE_ENTITY_ERROR = '351';
